60b3b52c04bced40717a6b8b6f4c14918d62b01f
Vanessa <yu100997@gmail.com>
2014-09-23 11:52:24 -0400

    added

---
+++ unittest/test.py
@@ -0,0 +1,34 @@
+import unittest
+
+'''
+validate password
+6-8 char
+1 num
+1 upper
+1 lower
+
+validate email
+thing @ thing . thing
+
+validate phone
+### ### ####
+##########
+###-###-####
+'''
+
+##############
+## Password ##
+##############
+
+class Test(unittest.TestCase):
+    def validatePass(self, str):
+        pass
+    
+    def passLenCheck(self, str):
+        pass
+    
+    def upperCheck(self, str):
+        pass
+
+    def lowerCheck(self, str):
+        pass
